3458913649da983f083aa791d08d0161f95d1d02
EvanBC1 <starfoxz13@gmail.com>
2024-07-15 15:18:11 -0700

    adding pending orders mockup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import ImageUpload from "./ImageUpload.jsx";
-
+import PendingOrders from "./Pages/PendingOrders/PendingOrders";
 
 function App() {
   // const [response, setResponse] = useState(null)
   return (
     <div className="App">
       <header className="App-header">
-        <ImageUpload />
+        {/* <ImageUpload /> */}
+        <PendingOrders />
       </header>
     </div>
   );
